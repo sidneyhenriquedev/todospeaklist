db9be69c2ddc8cfd4a54cc5b02d70aa57693d7f5
sidneyhenriquedev <sidneyhenrique.dev@gmail.com>
2024-01-27 23:47:23 -0300

    Update url of sounds

--- scripts/script.js
+++ scripts/script.js
@@ -79,7 +79,7 @@ function PressMicSaveList() {
 
 //Here sound when press btn record or press f9
 function recordingSound() {
-    //Comented for localhost
+    //Comented used in localhost for build the project
     // const AudioRecording = new Audio("../assets/recording.mp3")
     const AudioRecording = new Audio("https://sidneyhenriquedev.github.io/todospeaklist/assets/recording.mp3")
     AudioRecording.addEventListener('canplaythrough', (event) => {
@@ -89,7 +89,7 @@ function recordingSound() {
 
 
 function error_listening() {
-    const ErrorAudioListening = new Audio("../assets/error_listening.mp3")
+    const ErrorAudioListening = new Audio("https://sidneyhenriquedev.github.io/todospeaklist/assets/error_listening.mp3")
     ErrorAudioListening.addEventListener('canplaythrough', (event) => {
         ErrorAudioListening.play()
     })
